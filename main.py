56ca528eb1081eaf0d8602c0cd6cd2fa8837fe52
JBragaV <jocimarcaiadobraga@gmail.com>
2025-07-29 20:56:58 -0300

    Teste com as ações

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from utils import calculo_acao, calcular_pontos_de_trade, formatador_abertura_usuario
 
-lst_ticker = ['PETR4', 'VALE3', 'CPFE3', 'Wdofut', 'Winfut']
+lst_ticker_futuro = ['Wdofut', 'Winfut']
+lst_ticker_acoes = ['PETR4', 'VALE3', 'CPFE3']
+
 if __name__ == '__main__':
-    ticker = lst_ticker[2]
+    ticker = lst_ticker_acoes[0]
     if ticker in ['Wdofut', 'Winfut']:
         abertura_user = formatador_abertura_usuario(input(f"Qual a abertura do {ticker.upper()}"))
         df, _, desvio_padrao = calculo_acao(ticker)
         abertura = abertura_user
     else:
         df, abertura, desvio_padrao = calculo_acao(ticker)
-
     if abertura != 500 and not isinstance(abertura, int):
         print(df)
         lista_trade = calcular_pontos_de_trade(desvio_padrao, float(abertura))
