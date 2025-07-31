4bdaf828097e1b07cdf0f4061e2781416637f701
JBragaV <jocimarcaiadobraga@gmail.com>
2025-07-30 21:36:57 -0300

    Teste com o streamlit

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-from utils import calculo_acao, calcular_pontos_de_trade, formatador_abertura_usuario
+import streamlit as st
 
-lst_ticker_futuro = ['Wdofut', 'Winfut']
-lst_ticker_acoes = ['PETR4', 'VALE3', 'CPFE3']
+from utils import calculo_acao
 
-if __name__ == '__main__':
-    ticker = lst_ticker_acoes[0]
-    if ticker in ['Wdofut', 'Winfut']:
-        abertura_user = formatador_abertura_usuario(input(f"Qual a abertura do {ticker.upper()}"))
-        df, _, desvio_padrao = calculo_acao(ticker)
-        abertura = abertura_user
-    else:
-        df, abertura, desvio_padrao = calculo_acao(ticker)
-    if abertura != 500 and not isinstance(abertura, int):
-        print(df)
-        lista_trade = calcular_pontos_de_trade(desvio_padrao, float(abertura))
-        print(f'Ticker: {ticker}')
-        print(f'Abertura: {abertura}', f'Desvio Padrão: {desvio_padrao}')
-        for i, ponto in enumerate(lista_trade):
-            print(f'Ponto {i+1}: {abertura} + Desvio Padrão x {i+1}: {ponto[0]} '
-                  f'--- {abertura} - Desvio Padrão x {i+1}: {ponto[1]}')
-    else:
-        print(desvio_padrao)
+
+def home():
+    st.markdown('# B3 - Ações')
+    st.divider()
+    ticker = st.text_input('Insira o código da ação:')
+    btn_ticker = st.button('Calcular')
+    if btn_ticker:
+        df_ticker, abertura_hoje, desvio_padrao_arredondado = calculo_acao(ticker)
+        if abertura_hoje != 500:
+            col1, col2 = st.columns(2)
+            col1.metric(label=f'Abertura da {ticker.upper()}', value=abertura_hoje)
+            col2.metric(label=f'Desvio padrão da {ticker.upper()}', value=desvio_padrao_arredondado)
+            linha, _ = df_ticker.shape
+            st.markdown(f"## Quantidade de dias selecionada: {linha}")
+            st.dataframe(df_ticker)
+
+
+home()
---
+++ main_sem_st.py
@@ -0,0 +1,23 @@
+from utils import calculo_acao, calcular_pontos_de_trade, formatador_abertura_usuario
+
+lst_ticker_futuro = ['Wdofut', 'Winfut']
+lst_ticker_acoes = ['PETR4', 'VALE3', 'CPFE3']
+
+if __name__ == '__main__':
+    ticker = lst_ticker_acoes[0]
+    if ticker in ['Wdofut', 'Winfut']:
+        abertura_user = formatador_abertura_usuario(input(f"Qual a abertura do {ticker.upper()}"))
+        df, _, desvio_padrao = calculo_acao(ticker)
+        abertura = abertura_user
+    else:
+        df, abertura, desvio_padrao = calculo_acao(ticker)
+    if abertura != 500 and not isinstance(abertura, int):
+        print(df)
+        lista_trade = calcular_pontos_de_trade(desvio_padrao, float(abertura))
+        print(f'Ticker: {ticker}')
+        print(f'Abertura: {abertura}', f'Desvio Padrão: {desvio_padrao}')
+        for i, ponto in enumerate(lista_trade):
+            print(f'Ponto {i+1}: {abertura} + Desvio Padrão x {i+1}: {ponto[0]} '
+                  f'--- {abertura} - Desvio Padrão x {i+1}: {ponto[1]}')
+    else:
+        print(desvio_padrao)
